856c838b9ad9f0b82f005425e9e23903bcb84ad5
Kelly Duran <kaduran16@gmail.com>
2022-08-29 19:00:21 -0400

    commit tree 9fe5c276770948397d47d0f49019aedcd8ad77a6

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -18,6 +18,24 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  # PATCH /plants/:id
+  def update
+    plant = Plant.find_by(id: params[:id])
+    if plant
+      plant.update(plant_params)
+      render json: plant
+    end
+  end
+
+  # DELETE /plants/:id
+  def destroy 
+    plant = Plant.find_by(id: params[:id])
+    if plant
+      plant.destroy
+      head :no_content
+    end
+  end
+
   private
 
   def plant_params
